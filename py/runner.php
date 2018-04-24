210c05019bf3c9688f11fabfbb83157c27dda57f
bru02 <bru02@freemail.hu>
2018-04-24 15:50:05 +0200

    added stuff

--- py/runner.php
+++ py/runner.php
@@ -102,18 +102,18 @@
 	
 ?>
 <?php
-// function redirect($URL) {
-        // echo "<style>#loader{position:absolute;left:50%;top:50%;z-index:1;margin:-75px 0 0 -75px;border:16px solid #f3f3f3;border-radius:50%;border-top:16px solid #3498db;width:120px;height:120px;-webkit-animation:spin 2s linear infinite;animation:spin 2s linear infinite}@-webkit-keyframes spin{0%{-webkit-transform:rotate(0)}100%{-webkit-transform:rotate(360deg)}}@keyframes spin{0%{transform:rotate(0)}100%{transform:rotate(360deg)}}</style><div id='loader'></div>\n";
-        // echo "<script type='text/javascript'>document.location.href='{$URL}';</script>";
-// echo '<META HTTP-EQUIV="refresh" content="0;URL=' . $URL . '">';
-        // exit();
-   // }
-// if(!isset($_GET['f'])&&!isset($_REQUEST['run'])&&!isset($_REQUEST['stop'])) {
-	// redirect("manage.php");
-// } else {
-	// $f = htmlentities($_GET['f']);
-// }
-$f="asd/asd.py";
+function redirect($URL) {
+        echo "<style>#loader{position:absolute;left:50%;top:50%;z-index:1;margin:-75px 0 0 -75px;border:16px solid #f3f3f3;border-radius:50%;border-top:16px solid #3498db;width:120px;height:120px;-webkit-animation:spin 2s linear infinite;animation:spin 2s linear infinite}@-webkit-keyframes spin{0%{-webkit-transform:rotate(0)}100%{-webkit-transform:rotate(360deg)}}@keyframes spin{0%{transform:rotate(0)}100%{transform:rotate(360deg)}}</style><div id='loader'></div>\n";
+        echo "<script type='text/javascript'>document.location.href='{$URL}';</script>";
+echo '<META HTTP-EQUIV="refresh" content="0;URL=' . $URL . '">';
+        exit();
+   }
+if(!isset($_GET['f'])&&!isset($_REQUEST['run'])&&!isset($_REQUEST['stop'])) {
+	redirect("manage.php");
+} else {
+	$f = htmlentities($_GET['f']);
+}
+// $f="asd/asd.py";
 $e = explode("/",$f,1);
 ?>
 <!DOCTYPE html>
