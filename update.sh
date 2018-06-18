200836bbb635f19a3c51263837dfcd28e6fb076f
bru02 <bru02@freemail.hu>
2018-06-18 20:42:00 +0200

    aas

--- update.sh
+++ update.sh
@@ -5,7 +5,7 @@ sudo rm -R -f "$DIRECTORY"
     echo "Updating cod3r..."
 else
     echo "Installing cod3r via git"
-elif
+fi
 sudo mkdir "$DIRECTORY"
 sudo git clone https://github.com/bru02/ev3dev-cod3r.git "$DIRECTORY"
 echo "Finished. Now goto http://IpOfYourBrick/cod3r to see cod3r."
